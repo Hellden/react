a6093b433673c1fe60d4f720a9c27917f33d8f69
Hellden-XPS <rchapotin@gepy89.fr>
2018-12-13 22:21:47 +0100

    Add connexion

---
+++ project-3/src/components/NotFound.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NotFound = () => (
+  <h2 className='notFound'>Il n'y à rien ici</h2>
+)
+
+export default NotFound
--- project-3/src/index.js
+++ project-3/src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/* eslint-disable no-unused-expressions */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import Connexion from './components/Connexion'
+import NotFound from './components/NotFound'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from './serviceWorker'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+const Root = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={Connexion} />
+      <Route path='/pseudo/:pseudo' component={App} />
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
