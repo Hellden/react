85f5dacb8b67d491965f6a6ae651462ff5afc2c3
Hellden-XPS <rchapotin@gepy89.fr>
2018-12-14 15:42:04 +0100

    .

--- project-3/src/App.js
+++ project-3/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './App.css'
 import Formulaire from './components/Formulaire'
+import Message from './components/Message'
 
 class App extends Component {
   render () {
@@ -8,7 +9,9 @@ class App extends Component {
       <div className='box'>
         <div>
           <div className='messages'>
-          Message
+            <Message />
+            <Message />
+            <Message />
           </div>
         </div>
         <Formulaire />
