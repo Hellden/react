2158cc5170bcfca58b61c89a257dba6ce7e27f7b
Hellden-XPS <rchapotin@gepy89.fr>
2018-12-14 14:58:48 +0100

    .

--- project-3/src/App.js
+++ project-3/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 import './App.css'
+import Formulaire from './components/Formulaire'
 
 class App extends Component {
   render () {
     return (
       <div className='box'>
-        <h2>Titre</h2>
+        <div>
+          <div className='messages'>
+          Message
+          </div>
+        </div>
+        <Formulaire />
       </div>
     )
   }
